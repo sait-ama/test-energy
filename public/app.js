ef320e3bcc0e2e8b11ff2d1bb4b18cfcb8542418
sait-ama <126857264+sait-ama@users.noreply.github.com>
2026-07-12 15:55:31 +0500

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -2718,7 +2718,7 @@ function initSocket() {
   document.addEventListener('touchstart', sendHeartbeatNow);
 
   state.socket.on('connect', () => {
-    state.socket.emit('authenticate', { userId: state.user.id, version: '1.4.8' });
+    state.socket.emit('authenticate', { userId: state.user.id, version: '1.4.9' });
     startHeartbeat();
   });
 
@@ -2734,7 +2734,7 @@ function initSocket() {
       if (!state.socket.connected) {
         state.socket.connect();
       } else {
-        state.socket.emit('authenticate', { userId: state.user.id, version: '1.4.8' });
+        state.socket.emit('authenticate', { userId: state.user.id, version: '1.4.9' });
         sendHeartbeatNow();
       }
     }
